f33a8c6c8c827c2a629e7bd0a19a05a76121513c
romantrety <114093642+22057221@users.noreply.github.com>
2022-10-12 18:41:38 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//for 1
+	//int n, k;
+	//cout << "n:";
+	//cin >> n;
+
+	//cout << "k:";
+	//cin >> k;
+	//for (int i = 0; i < n; ++i) {
+	//	cout << k << endl;
+	//}
+//}
+
+
+
+
+	//for 2
+	//int a, b;
+	//int n = 0;
+	//cout << "a: ";
+	//cin >> a;
+	//cout << "b: ";
+	//cin >> b;
+	//for (int i = a; i <= b; i = i + 1) {
+	//	cout << i << endl;
+	//	n++;
+	//}
+	//cout << "gty = " << n << endl;
+//}
+
+	//" стоимость конфет:  "
+	//"стоимость одного кг конфет: "
+
+	//for 4
+	//double price;
+	//cout << "input price: ";
+	//cin >> price;
+	//for (int weight = 1; weight <= 10; weight++) {
+	//	cout << weight << " kg coats " << weight * price << " $" << endl;
+	//}
+//}
+
+
+
+	//for 5
+	//double a = 1;
+	//for (double = 0.1; m < 1.1; m + 0.1)
+		//cout << m << " kg = " << c * m << endl;
+//}
+
+
+
+	//for 6
+//	double a = 1;
+//
+//	for (double b = 1; b <= 2; b = b + 0.2) {
+//		cout << b << "kg = " << b * a << endl;
+//	}
+
+
+	//for 7 
+	//int a, b;
+	//cin >> a;
+	//cin >> b;
+	//int sum = 0;
+	//for (int i = a; i <= b; ++i)
+	//	sum += i;
+	//cout << "Sum = " << sum;
+//}
+
+
+
+//for 8
+	//int a, b, c = 1;
+	//cin >> a;
+	//cin >> b;
+	//for (int i = a; i <= b; i++) {
+	//	c *= i;
+	//}
+	//cout << c;
+//}
